18bccf5130981f961b5890a94cf42c86f48624a3
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-07-15 00:17:29 +0100

    [feature] devconnector

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -159,7 +159,7 @@ router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session:
     Post.findById(req.params.id)
     .then(post => {
         //check if comment exist
-        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id.length === 0){
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
             return res.status(404).json({commentnotexist: 'Comment does not exist'});
         }
 
@@ -172,5 +172,7 @@ router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session:
         post.comments.splice(removeIndex, 1);
         post.save().then(post => res.json(post));
     })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+
 });
 module.exports = router;
