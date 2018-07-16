4edf63418a4a06f4ede3d8f4531c3e38b7691f44
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-07-16 07:49:57 +0100

    [feature] devconnector

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-       <h1>Dev connector</h1>
+      <Navbar />
+      <Route exact path = "/" component = {Landing} />
+      <div className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
+       <Footer />
+      </div>
+      </Router>
     );
   }
 }
