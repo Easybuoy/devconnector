0e8183ff605d6661b0e60deedca13d076119549c
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-08-03 19:03:10 +0100

    Prepared for deploy

--- app.js
+++ app.js
@@ -50,10 +50,12 @@ if(process.env.NODE_ENV === 'production'){
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    }); 
 }
 
-console.log(process.env);
+console.log(process.env.NODE_ENV);
+console.log(process.env.MONGO_URI);
+console.log(process.env.SECRET_OR_KEY);
 
 const port = process.env.PORT || 4000;
 
